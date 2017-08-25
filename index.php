be9ff8a18f7edee61eb1186bdbc1a417af386d2d
Verta-master <vertyagina@gmail.com>
2017-08-25 11:22:50 +0600

    Task 4

--- index.php
+++ index.php
@@ -122,26 +122,38 @@ $days_until_deadline = null;
 
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input id="show-complete-tasks" class="checkbox__input visually-hidden" type="checkbox">
+                        <?php
+                            if ($show_complete_tasks == 1) {
+                                print('<input id="show-complete-tasks" class="checkbox__input visually-hidden" type="checkbox" checked>');
+                            } else {
+                                print('<input id="show-complete-tasks" class="checkbox__input visually-hidden" type="checkbox">');
+                            }
+                        ?>
+
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
 
                 <table class="tasks">
-
                     <!--показывать следующий тег <tr/>, если переменная равна единице-->
-                    <tr class="tasks__item task task--completed">
-                        <td class="task__select">
-                            <label class="checkbox task__checkbox">
-                                <input class="checkbox__input visually-hidden" type="checkbox" checked>
-                                <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
-                            </label>
-                        </td>
-                        <td class="task__date">10.04.2017</td>
+                    <?php
+                        if ($show_complete_tasks == 1) {
+                            print('
+                                <tr class="tasks__item task task--completed">
+                                    <td class="task__select">
+                                        <label class="checkbox task__checkbox">
+                                            <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                            <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                                        </label>
+                                    </td>
+                                    <td class="task__date">10.04.2017</td>
 
-                        <td class="task__controls">
-                        </td>
-                    </tr>
+                                    <td class="task__controls">
+                                    </td>
+                                </tr>
+                            ');
+                        }
+                    ?>
 
                     <tr class="tasks__item task">
                         <td class="task__select">
