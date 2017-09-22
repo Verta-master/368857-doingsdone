c3294e55118929768ce545e04b9ae0c5f21269e4
Verta-master <vertyagina@gmail.com>
2017-09-22 14:59:26 +0600

    SQL Functions

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 require_once "mysql_helper.php";
+
+//Функция загрузки шаблона
 function renderTemplate($path, $array) {
 
     if (file_exists($path)) {
@@ -12,4 +14,40 @@ function renderTemplate($path, $array) {
         return "";
     }
 }
+
+//Функция для получения данных
+function select_data($con, $sql, $array = []) {
+    $stmt = db_get_prepare_stmt($con, $sql, $array);
+    $exec = mysqli_stmt_execute($stmt);
+    if ($exec) {
+        $result = mysqli_fetch_all($stmt, MYSQLI_ASSOC);
+    } else {
+        $result = [];
+    }
+    return $result;
+}
+
+//Функция для вставки данных
+function insert_data($con, $table, $array) {
+    foreach ($array as $key => $val) {
+        $line .= $key."='".$val."', ";
+    }
+    $sql = "INSERT INTO" . $table . "SET " . $line;
+    $stmt = db_get_prepare_stmt($con, $sql, $array);
+    $exec = mysqli_stmt_execute($stmt);
+    if ($exec) {
+        $result = mysqli_insert_id($con);
+    } else {
+        $result = false;
+    }
+    return $result;
+}
+
+//Функция для произвольного запроса
+function exec_query($con, $sql, $array = []) {
+    $stmt = db_get_prepare_stmt($con, $sql, $array);
+    $exec = mysqli_stmt_execute($stmt);
+    return $exec;
+}
+
 ?>
